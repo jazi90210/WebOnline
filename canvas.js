276d9f802a7021733c0e94d934f1734cdaeb2e33
jazi90210 <jahazi90@hotmail.com>
2014-08-21 19:36:18 -0700

    quitar alert

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 var dibujo, lienzo, t, b;
 function inicio()
 {
-    alert("Todo funciona");
+    var a = 25;
     t = document.getElementById("usuario");
     b = document.getElementById("dibujalo");a
     dibujo = document.getElementById("dibujito");
